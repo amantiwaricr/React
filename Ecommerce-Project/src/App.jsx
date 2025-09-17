868ce5e3eef9b3dd3df1c749834e76123e65ba43
amantiwaricr <amanbkvm2019@gmail.com>
2025-09-17 18:00:30 +0545

    Added OrderPage and TrackingPage

--- Ecommerce-Project/src/App.jsx
+++ Ecommerce-Project/src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router'
 import { HomePage } from './Pages/HomePage'
 import { CheckoutPage } from './Pages/CheckoutPage'
+import { OrderPage } from './Pages/OrderPage'
+import { TrackingPage } from './Pages/TrackingPage'
 import './App.css'
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <Routes>
     <Route index element={ <HomePage />}  />
     <Route path='checkout' element={ <CheckoutPage />} />
+    <Route path='Order' element={ <OrderPage />} />
+    <Route path='Tracking' element={ <TrackingPage />} />
     </Routes>
    
   )
