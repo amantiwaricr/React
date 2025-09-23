af55cc0d051894913c2a50c499227b4315263732
amantiwaricr <amanbkvm2019@gmail.com>
2025-09-23 15:37:35 +0545

    generate HTML for cart items

--- Ecommerce-Project/src/App.jsx
+++ Ecommerce-Project/src/App.jsx
@@ -8,24 +8,24 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-   const [cart, setCart] = useState([]); 
+  const [cart, setCart] = useState([]);
 
-      useEffect(() => {
-             axios.get('/api/cart-items')
+  useEffect(() => {
+    axios.get('/api/cart-items?expand=product')
       .then((response) => {
         setCart(response.data)
       });
-      }, [])
-     
+  }, [])
+
 
   return (
     <Routes>
-    <Route index element={ <HomePage  cart={cart} />}  />
-    <Route path='checkout' element={ <CheckoutPage cart={cart} />} />
-    <Route path='Orders' element={ <OrderPage />} />
-    <Route path='Tracking' element={ <TrackingPage />} />
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path='checkout' element={<CheckoutPage cart={cart} />} />
+      <Route path='Orders' element={<OrderPage />} />
+      <Route path='Tracking' element={<TrackingPage />} />
     </Routes>
-   
+
   )
 }
 
