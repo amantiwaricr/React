39c6cc6a148a58bcca68a6d663f6188d675b1ee0
amantiwaricr <amanbkvm2019@gmail.com>
2025-09-24 18:42:15 +0545

    Generate the html FOR THE ORDER PAGE

--- Ecommerce-Project/src/App.jsx
+++ Ecommerce-Project/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
     <Routes>
       <Route index element={<HomePage cart={cart} />} />
       <Route path='checkout' element={<CheckoutPage cart={cart} />} />
-      <Route path='Orders' element={<OrderPage />} />
+      <Route path='Orders' element={<OrderPage cart={cart} />} />
       <Route path='Tracking' element={<TrackingPage />} />
     </Routes>
 
